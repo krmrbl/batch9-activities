ea627068a3547d2475544eb267b7f1929b088a77
krmrbl <koodlem@gmail.com>
2021-05-11 21:28:10 +0800

    Coding Challenge 2 Answer

--- bool-kiara/cloned-repo/js-challenges-and-projects-v.1.2/intro-to-loops/be-the-bouncer!/app.js
+++ bool-kiara/cloned-repo/js-challenges-and-projects-v.1.2/intro-to-loops/be-the-bouncer!/app.js
@@ -24,3 +24,22 @@ const nightClubRegister = [
         gender: 'male'
     }
 ]
+
+for (i = 0; i < nightClubRegister.length; i++) {
+    if(nightClubRegister[i].age < 18) {
+        if(nightClubRegister[i].gender === 'female') {
+            console.log('Sorry, Ms. ' + nightClubRegister[i].name + ' ' + nightClubRegister[i].lastname + '. You can not enter because you are underage.');
+        }
+        else if(nightClubRegister[i].gender === 'male') {
+            console.log('Sorry, Mr. ' + nightClubRegister[i].name + ' ' + nightClubRegister[i].lastname + '. You can not enter because you are underage.');
+        }
+    }
+    else {
+        if(nightClubRegister[i].gender === 'female') {
+            console.log('Welcome, Ms. ' + nightClubRegister[i].name + ' ' + nightClubRegister[i].lastname + '.');
+        }
+        else if(nightClubRegister[i].gender === 'male') {
+            console.log('Welcome, Mr. ' + nightClubRegister[i].name + ' ' + nightClubRegister[i].lastname + '.');
+        }
+    }
+}
